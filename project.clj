a75623c519843bb6ffc442ff91e97948463b57e3
Eric Schulte <schulte.eric@gmail.com>
2010-02-16 22:14:47 -0700

    removing incanter from the project dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "Genetic Programming on assembly source code"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [incanter/incanter-full "1.0-master-SNAPSHOT"]
                  [org.apache/bcel/bcel "5.3-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
