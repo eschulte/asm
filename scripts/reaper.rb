4f1ff734583afa8d4d28d91de790698b2cb9b799
Eric Schulte <schulte.eric@gmail.com>
2010-02-24 14:32:31 -0700

    the reaper will now take you and all your descendants

--- scripts/reaper.rb
+++ scripts/reaper.rb
@@ -1,21 +1,19 @@
 #!/usr/bin/env ruby
-## reap long-running processes
+## reap long-running processes and all of their descendants
 ##
 ##   (?_^
 ##    |\ )
 ##    |/_\
 ##
-def variant_procs
-  %x{ps auxwww |grep -i "tmp/variant"}.split("\n").map{ |l| $1 if l.match(/^[\S]+[\s]+([\d]+)[\s]+/) }
+def variants
+  %x{ps auxwww |grep -i "tmp/variant"}.split("\n").map{|l| $1 if l.match(/^[\S]+[\s]+([\d]+)[\s]+/)}
+end
+def and_descendants(pid)
+  %x{ps -axo pid,ppid |grep -i #{pid}}.split("\n").map{|l| $1 if l.match(/^([\d]+)[\s]+([\d]+)/)}
 end
 old_procs = []
 while true do
-  # say it
-  # puts :collecting
-  # puts variant_procs.size
-  # variant_procs.select{ |p| old_procs.include?(p) }.each{ |p| puts p }
-  # do it
-  variant_procs.select{ |p| old_procs.include?(p) }.each{ |p| %x{kill -9 #{p}} }
+  variants.select{|p| old_procs.include?(p)}.each{|p| p.and_descendants.each{|p| %x{kill -9 #{p}}}}
   old_procs = variant_procs
   sleep 4
 end
