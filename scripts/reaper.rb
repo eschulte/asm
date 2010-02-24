56229de61e3bf1582a97e94b2271bc1f6f2718a4
Eric Schulte <schulte.eric@gmail.com>
2010-02-24 14:40:56 -0700

    reaper is working

--- scripts/reaper.rb
+++ scripts/reaper.rb
@@ -6,14 +6,14 @@
 ##    |/_\
 ##
 def variants
-  %x{ps auxwww |grep -i "tmp/variant"}.split("\n").map{|l| $1 if l.match(/^[\S]+[\s]+([\d]+)[\s]+/)}
+  %x{ps auxwww |grep -i "tmp/variant"}.split("\n").map{|l| $1 if l.match(/^[\S]+[\s]+([\d]+)[\s]+/)}.compact
 end
 def and_descendants(pid)
-  %x{ps -axo pid,ppid |grep -i #{pid}}.split("\n").map{|l| $1 if l.match(/^([\d]+)[\s]+([\d]+)/)}
+  %x{ps ax -o pid,ppid |grep -i #{pid}}.split("\n").map{|l| $1 if l.match(/^([\d]+)[\s]+([\d]+)/)}.compact
 end
 old = []
 while true do
-  variants.select{|p| old.include?(p)}.each{|p| p.and_descendants.each{|p| %x{kill -9 #{p}}}}
+  variants.select{|p| old.include?(p)}.each{|p| and_descendants(p).each{|p| %x{kill -9 #{p}}}}
   old = variants
   sleep 4
 end
