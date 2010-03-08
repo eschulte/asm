8f1d2f4b76959e04bc01b03b5dfc4197c10c0c6a
Eric Schulte <schulte.eric@gmail.com>
2010-03-08 13:53:43 -0700

    fixed bug in the reaper

--- scripts/reaper.rb
+++ scripts/reaper.rb
@@ -7,7 +7,12 @@
 ##
 def variants
   ["tmp/variant", "exec_bad", "exec_good"].map do |pattern|
-    %x{ps auxwww |grep -i pattern}.split("\n").map{|l| $1 if l.match(/^[\S]+[\s]+([\d]+)[\s]+/)}.compact
+    %x{ps auxwww |grep -i #{pattern}}.split("\n").map do |l|
+      if l.match(/^[\S]+[\s]+([\d]+)[\s]+/)
+        # puts l
+        $1
+      end
+    end.compact
   end.flatten
 end
 def and_descendants(pid)
@@ -16,6 +21,7 @@ end
 old = []
 while true do
   variants.select{|p| old.include?(p)}.each{|p| and_descendants(p).each{|p| %x{kill -9 #{p}}}}
+  # variants.select{|p| old.include?(p)}.each{|p| and_descendants(p).each{|p| puts %Q{kill -9 #{p}}}}
   old = variants
   sleep 4
 end
