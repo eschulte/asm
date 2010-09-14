ffab79d43af543ef08d0e75278cbf6ae3a464d3d
Eric Schulte <schulte.eric@gmail.com>
2010-09-14 17:35:22 -0400

    re-organize the repository with the new lein and for Clojure 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject asm-gp "1.0.0-SNAPSHOT"
-  :description "Genetic Programming on assembly source code"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+(defproject epr-asm "1.0.0-SNAPSHOT"
+  :description "evolutionary program repair over assembly code"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache/bcel/bcel "5.3-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
